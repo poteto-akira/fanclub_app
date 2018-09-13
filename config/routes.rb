18812db5a36431f3b5b0727f8a3ef6d44c231566
poteto-akira <poteto.akira@gmail.com>
2018-09-13 16:39:50 +0900

    routes.rbのindexのルート直した

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   post "likes/:post_id/create" => "likes#create"
   post "likes/:post_id/destroy" => "likes#destroy"
 
-  get "/" => 'home#index'
+  root 'home#index'
   get "home/new_fc" => "home#new_fc"
   post "home/create" => "home#create"
   get "home/profile/:id" => "home#profile"
