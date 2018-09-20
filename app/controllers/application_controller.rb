52c7c80b36423e36c28e12e9991e501f63278167
poteto-akira <poteto.akira@gmail.com>
2018-09-20 16:54:31 +0900

    edit redirect ログインしていない状態で入会ボタンをクリックして会員登録をした後のリダイレクト先をログイン直前のページに設定する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,27 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :set_current_user
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :store_location
+
+
+  def store_location
+    if (request.fullpath != "/users/sign_in" &&
+      request.fullpath != "/users/sign_up" &&
+      request.fullpath !~ Regexp.new("\\A/users/password.*\\z") &&
+      !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    if (session[:previous_url] == root_path)
+      super
+    else
+      session[:previous_url] || root_path
+    end
+  end
+
 
   protected
 
