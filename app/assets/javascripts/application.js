06129a39e209bcb424076ea135e3007bdd785c23
poteto-akira <poteto.akira@gmail.com>
2018-09-23 02:03:38 +0900

    make snap in fc_content/index: fc_content/indexni の会員タイムラインを右からスワイプ表示。タイムラインはスクロール機能追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,25 @@
 
 //= require turbolinks
 //=require_tree
+
+<script type="text/javascript">
+
+// 基本的な記述です。コンテンツ部分のID名を書いておきましょう。
+var snapper = new Snap({
+  element: document.getElementById('content')
+});
+
+// 以下はフリックやドラッグだけでなく、ボタンでも開いたり閉じたりできるようにする記述です。
+var addEvent = function addEvent(element, eventName, func) {
+  if (element.addEventListener) {
+      return element.addEventListener(eventName, func, false);
+  } else if (element.attachEvent) {
+      return element.attachEvent("on" + eventName, func);
+  }
+};
+
+// ボタンのID名と揃えておきましょう。
+addEvent(document.getElementById('open-right'), 'click', function(){
+  snapper.open('right');
+});
+</script>
