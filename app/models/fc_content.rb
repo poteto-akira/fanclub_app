8d3307797952df1b2225450e271023812bc33f10
poteto-akira <poteto.akira@gmail.com>
2018-10-01 19:18:47 +0900

    edit fc_content

--- app/models/fc_content.rb
+++ app/models/fc_content.rb
@@ -2,7 +2,7 @@ class FcContent < ApplicationRecord
   # validates :fc_id,{presence: true}
   validates :content_body, {presence: true}
   # controllerでdescできなかったからここで新着順に並び替えている
-  default_scope -> { order(created_at: :desc) }
+  # default_scope -> { order(created_at: :desc) }
   belongs_to :user
   # belongs_to :post
 
