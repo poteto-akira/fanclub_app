f0588afca29cc6dd8bc67203c81e454cdcde8d21
poteto-akira <poteto.akira@gmail.com>
2018-10-29 15:20:30 +0900

    modify code

--- app/controllers/fc_contents_controller.rb
+++ app/controllers/fc_contents_controller.rb
@@ -2,59 +2,42 @@ class FcContentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # render :layout => "index.html.erb"
+    # URLにあるPost.idを取得する
     @post = Post.find_by(id: params[:id])
-    # ここでorderしてもダメだからfc_content.rbで書いている
-    # @contents = FcContent.all.order(created_at: :desc)
-    @content = FcContent.new
-    # @contents = @post.content
-    # @contents = FcContent.all
-    # respond_to do |format|
-    #       format.html # html形式でアクセスがあった場合は特に何もなし(@messages = Message.allして終わり）
-    #       format.json { @new_content = FcContent.where('id > ?', params[:id]) } # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する
-    #     end
-    @user = User.find_by(id: params[:id])
-
   end
 
   def show
     @post = Post.find_by(id: params[:id])
     @content = FcContent.find_by(id: params[:id])
     @content = FcContent.new
-    # @contents = @post.contents
-
   end
 
   def new
-    @content = FcContent.find_by(id: params[:id])
-    @post = Post.find_by(id: params[:id])
+    # @content = FcContent.find_by(id: params[:id])
+    # @post = Post.find_by(id: params[:id])
   end
 
   def create
     @post = Post.find_by(id: params[:id])
-    # form_tagに@post.idを含ませて現在のfc.idを取得している
+    # form_tagに@post.idを含ませて現在閲覧中のfc.idを取得している
     @content = FcContent.new(content_body: params[:content_body],
                              user_id: current_user.id,
                              post_id: params[:id],
                              username: current_user.name)
     respond_to do |format|
-    if @content.save
-    # saveの後にpost.idがついたurlに戻る
-      # redirect_to("/fc_contents/index/#{@post.id}")
-      # render("index")
-      format.html
-      format.js
-    else
-      format.js {render :index}
+      if @content.save
+        format.html
+        format.js #index.js.htmlに移動
+      else
+        format.js {render :index}
+      end
     end
-  end
   end
 
   private
     def content_params
-      params.require(:content).permit(:content_content_body, :post_id, :user_id)
+      params.require(:content).permit(:content_content_body, :post_id, :user_id, :username)
     end
-
 
     def fccontent_params
       params.require(:fc_content).permit(:content_body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  # get '/users', to: redirect("/users/sign_up")
 
+  # ============ devise ==============
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
 
-
-  # ============fc_content.controller ==============
-  get 'fc_contents/index/:id' => "fc_contents#index"
-  get "fc_contents/new/:id" => "fc_contents#new"
-  get "fc_content/:id" => "fc_contents#show"
-  post "fc_content/create/:id" => "fc_contents#create"
-
-  post "enter/:id/create" => "enters#create"
+  # ============ home.controller ==============
+  root 'home#index'
   get ":id/enters" => "home#enters"
   get ":post_id/fcshow" => "home#show"
   get ":id/edit_profile" => "home#edit_profile"
@@ -19,13 +13,7 @@ Rails.application.routes.draw do
   get "user/:id/fc_list" => "home#fc_list"
   get "others" => "home#others"
   get "policy_law" => "home#policy_law"
-  get 'pay/payment'
-
   get "home/:id/likes" => "home#likes"
-  post "likes/:post_id/create" => "likes#create"
-  post "likes/:post_id/destroy" => "likes#destroy"
-
-  root 'home#index'
   get "home/new_fc" => "home#new_fc"
   post "home/create" => "home#create"
   get "home/profile/:id" => "home#profile"
@@ -34,7 +22,21 @@ Rails.application.routes.draw do
   post ":id/update_fc" => "home#update_fc"
   post ":id/fc/destroy" => "home#destroy"
   get ":id/setting" => "home#setting"
-  get "pay" => "pay#payment"
   get "home/new_fc_content" => "home#new_fc_content"
   post "home/content_create" => "home#content_create"
+
+  # ============ fc_content.controller ==============
+  get 'fc_contents/index/:id' => "fc_contents#index"
+  get "fc_contents/new/:id" => "fc_contents#new"
+  get "fc_content/:id" => "fc_contents#show"
+  post "fc_content/create/:id" => "fc_contents#create"
+
+
+  post "enter/:id/create" => "enters#create"
+
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
+
+  get "pay" => "pay#payment"
+
 end
