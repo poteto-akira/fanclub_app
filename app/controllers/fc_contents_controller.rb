29449ca2e8fc088cf2123efc3c51cf748f5ac421
poteto-akira <poteto.akira@gmail.com>
2019-03-25 00:25:16 +0900

    refactor: remove unused code

--- app/controllers/fc_contents_controller.rb
+++ app/controllers/fc_contents_controller.rb
@@ -5,17 +5,9 @@ class FcContentsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
 
-  def show
-    @post = Post.find_by(id: params[:id])
-    @content = FcContent.find_by(id: params[:id])
-    @content = FcContent.new
-  end
-
-  def new
-  end
-
   def create
     @post = Post.find_by(id: params[:id])
+    # TODO: パラメータをprivateメソッドにする
     @content = FcContent.new(content_body: params[:content_body],
                              user_id: current_user.id,
                              post_id: params[:id],
@@ -23,19 +15,10 @@ class FcContentsController < ApplicationController
     respond_to do |format|
       if @content.save
         format.html
-        format.js #index.js.htmlに移動
+        format.js
       else
         format.js {render :index}
       end
     end
   end
-
-  private
-    def content_params
-      params.require(:content).permit(:content_content_body, :post_id, :user_id, :username)
-    end
-
-    def fccontent_params
-      params.require(:fc_content).permit(:content_body)
-    end
 end
