f3a10dddb7c13bf2c7bda9913843baf9f503083d
poteto-akira <poteto.akira@gmail.com>
2019-03-26 23:28:14 +0900

    refactor: remove unused comments

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,8 +2,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
-    if user.persisted?  # ユーザーがデータベース上に存在している。
-      sign_in_and_redirect user  # ユーザーをsign_inする
+    if user.persisted?
+      sign_in_and_redirect user
       set_flash_message(:notice, :success, kind: __callee__.to_s.capitalize) if is_navigational_format?
     else  # 何らかの理由でデータベースに保存されていない。
       session["devise.user_attributes"] = user.attributes  # 認証データを覚えておく。
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,81 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  # 画像をアップロードできるようにしている
-  # mount_uploader :image_name, ImagesUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:timeoutable, :omniauthable, omniauth_providers: [:twitter]
-
   validates :name, {presence: true}
-
   has_many :posts, dependent: :destroy
   has_many :fc_contents, dependent: :destroy
-  # has_many :fc_name
-
 
   def posts
     return Post.where(user_id: self.id)
   end
 
-# twitterログイン関連
-  # def self.find_or_create_from_auth_hash(auth_hash)
-  #  provider = auth_hash[:provider] #providerはどのサービスで認証したのかを見分けるもの
-  #  uid = auth_hash[:uid]
-  #  name = auth_hash[:info][:name]
-  #  image_url = auth_hash[:info][:image]
-  #
-  #  #find_or_create_by()は()の中の条件のものが見つければ取得し、なければ新しく作成するというメソッド
-  #  self.find_or_create_by(provider: provider,uid: uid) do |user|
-  #    user.username = name
-  #    user.image_url = image_url
-  #  end
-  # end
-
-# twitterログイン関連
-  # def self.from_omniauth(auth)
-  #   find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
-  #     user.provider = auth["provider"]
-  #     user.uid = auth["uid"]
-  #     user.username = auth["info"]["nickname"]
-  #   end
-  # end
-  #
-  # def self.new_with_session(params, session)
-  #   if session["devise.user_attributes"]
-  #     new(session["devise.user_attributes"]) do |user|
-  #       user.attributes = params
-  #     end
-  #   else
-  #     super
-  #   end
-  # end
-
-  # def self.find_for_oauth(auth)
-  #   user = User.where(uid: auth.uid, provider: auth.provider).first
-  #
-  #   unless user
-  #     user = User.create(
-  #       uid:      auth.uid,
-  #       provider: auth.provider,
-  #       email:    User.dummy_email(auth),
-  #       password: Devise.friendly_token[0, 20],
-  #       image: auth.info.image,
-  #       name: auth.info.name,
-  #       nickname: auth.info.nickname,
-  #       location: auth.info.location
-  #     )
-  #   end
-  #
-  #   user
-  # end
-  #
-  # private
-  #
-  # def self.dummy_email(auth)
-  #   "#{auth.uid}-#{auth.provider}@example.com"
-  # end
-
-  # OmniAuth認証データを元にデータベースでユーザーを探す。なければ新しく作る。
     def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.provider = auth.provider
@@ -83,10 +16,7 @@ class User < ApplicationRecord
         user.name = auth.info.name
         user.username = auth.info.nickname
         user.remote_image_name_url = auth.info.image
-        # DBに仮のメアドを保存
         user.email = auth["email"].nil? ? auth["provider"] + auth["uid"] + "@login.function" : auth["email"]
-
-        # パスワード不要なので、パスワードには触らない。
       end
     end
 
