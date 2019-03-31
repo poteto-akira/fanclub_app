1614007c43ea54f53201d57ae04956f3e5fbec01
poteto-akira <poteto.akira@gmail.com>
2019-04-01 00:28:17 +0900

    refactor: remove new line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,7 @@ class ApplicationController < ActionController::Base
   after_action  :store_location
 
   def store_location
-    if (request.fullpath != "/users/sign_in" &&
-      request.fullpath != "/users/sign_up" &&
-      request.fullpath !~ Regexp.new("\\A/users/password.*\\z") &&
-      !request.xhr?)
+    if (request.fullpath != "/users/sign_in" && request.fullpath != "/users/sign_up" && request.fullpath !~ Regexp.new("\\A/users/password.*\\z") && !request.xhr?)
       session[:previous_url] = request.fullpath
     end
   end
